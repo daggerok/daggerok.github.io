5c55754d857250f7239e95bb7e7b7d16ff8576f8
Maksim Kostromin <daggerok@gmail.com>
2025-02-08 13:34:02 -0500

    Add bun new script for quicker template creation for both en and ru posts.

---
+++ src/new.ts
@@ -0,0 +1,44 @@
+// perse path argument
+import { path, fs } from "@vuepress/utils";
+
+if (process.argv.length <= 2) {
+  console.log('Script requires argument for path post to be created');
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDay()).padStart(2, '0');
+  console.log(`
+    Usage:
+      bun new ${year} ${year}-${month}-${day}-Hello-World
+  `);
+  process.exit(1);
+}
+
+const [exec, script, ...rest] = process.argv;
+const joinedString = rest.join(' ');
+
+const blogDir = path.join(__dirname, 'blog', ...rest);
+fs.mkdirpSync(blogDir);
+
+const blobPost = path.join(blogDir, 'README.md');
+fs.writeSync(fs.openSync(blobPost, 'w'), `# TODO ${joinedString}
+${joinedString}
+
+[[toc]]
+`);
+console.log(`file ${blobPost} created`);
+
+const ruBlogDir = path.join(__dirname, 'ru', 'blog', ...rest);
+fs.mkdirpSync(ruBlogDir);
+
+const ruBlobPost = path.join(ruBlogDir, 'README.md');
+fs.writeSync(fs.openSync(ruBlobPost, 'w'), `---
+lang: ru-RU
+---
+
+# В разработке ${joinedString}
+${joinedString}
+
+[[toc]]
+`);
+console.log(`file ${ruBlobPost} created`);
